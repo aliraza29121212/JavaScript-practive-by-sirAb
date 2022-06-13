d4ace08bfd369ea2f0d6212f2051d2f59e6e872f
aliraza29121212 <95703064+aliraza29121212@users.noreply.github.com>
2022-06-13 09:06:36 -0700

    add

--- lacture-1/main.js
+++ lacture-1/main.js
@@ -7,6 +7,16 @@
 //   }
 // };
 
+// let Clicked = () => {
+//   try {
+//     let SelectedObject = document.querySelector("h1").innerHTML;
+
+//     alert(SelectedObject);
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// };
+
 // let ABC = () => {
 //   try {
 //     document.querySelector("h1").innerHTML = "Value Change ho gai ha";
@@ -15,20 +25,70 @@
 //   }
 // };
 
-let ABC = () => {
-  try {
-    let SelectedObject = document.querySelector("h1");
-    SelectedObject.innerHTML = "hey";
-    SelectedObject.style.color = "red";
-  } catch (error) {
-    alert(error.message);
-  }
-};
+// let Clicked = () => {
+//   try {
+//     document.querySelector("h1").innerHTML = "HEyy THere";
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// };
 
-let ChangeImage = () => {
+// let ABC = () => {
+//   try {
+//     let SelectedObject = document.querySelector("h1");
+//     SelectedObject.innerHTML = "hey";
+//     SelectedObject.style.color = "red";
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// };
+// let Clicked = () => {
+//   try {
+//     let SelectedObject = document.querySelector("h1");
+//     SelectedObject.innerHTML = "Hii World";
+//     SelectedObject.style.color = "red";
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// };
+
+// let ChangeImage = () => {
+//   try {
+//     let SelectedObject = document.querySelector(".FirstImage");
+//     SelectedObject.src = "img/002.png";
+//   } catch (error) {
+//     error.message;
+//   }
+// };
+
+// let ChangeImage = () => {
+//   try {
+//     let SelectedObject = document.querySelector(".FirstImage");
+//     SelectedObject.src = "img/002.png";
+//   } catch (error) {
+//     error.message;
+//   }
+// };
+
+// let visibleText = () => {
+//   try {
+//     let selectedObject = document.querySelector(".visibleText");
+//     let Element = document.createElement("h1");
+//     let InnerHtml = document.createTextNode("Ali Raza Here");
+//     Element.appendChild(InnerHtml);
+//     selectedObject.appendChild(Element);
+//   } catch (error) {
+//     error.message;
+//   }
+// };
+
+let visibleText = () => {
   try {
-    let SelectedObject = document.querySelector(".FirstImage");
-    SelectedObject.src = "img/002.png";
+    let SelectedObject = document.querySelector(".InsideDiv");
+    let Element = document.createElement("h1");
+    let InnerHtml = document.createTextNode("This is Ali Raza from own Brand");
+    Element.appendChild(InnerHtml);
+    SelectedObject.appendChild(Element);
   } catch (error) {
     error.message;
   }
